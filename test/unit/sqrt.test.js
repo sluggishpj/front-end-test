956812650d40925cc94a26931a94116ab3544e17
xupeijie <xupeijie@bigo.sg>
2018-08-17 19:41:12 +0800

    test

--- test/unit/sqrt.test.js
+++ test/unit/sqrt.test.js
@@ -7,9 +7,9 @@ describe('sqrt', function() {
         expect(My.sqrt(4)).to.equal(2)
     })
 
-    it('参数为负值时应该报错', function() {
-        expect(function() {
-            My.sqrt(-1)
-        }).to.throw('负值没有平方根')
-    })
+    // it('参数为负值时应该报错', function() {
+    //     expect(function() {
+    //         My.sqrt(-1)
+    //     }).to.throw('负值没有平方根')
+    // })
 })
